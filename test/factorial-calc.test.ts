de85bcfdf2329283b77a03200316b9eba582fb71
Edi Recica <edi@recica.nl>
2021-12-12 19:33:06 +0100

    Initial commit

---
+++ test/factorial-calc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FactorialCalc from '../lib/factorial-calc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/factorial-calc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FactorialCalc.FactorialCalcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
